3fc29f64dac07c65200b4f50fde772fa87287724
MrSterdy <mrsterdy@gmail.com>
2023-08-12 20:51:39 +0400

    Disable prerender

--- svelte.config.js
+++ svelte.config.js
@@ -6,10 +6,7 @@ const config = {
     preprocess: vitePreprocess(),
 
     kit: {
-        adapter: adapter({
-            precompress: true,
-            polyfill: false
-        })
+        adapter: adapter()
     }
 };
 
