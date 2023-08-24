46fa82766fed446cd0b87c58e09bdfa3254ae716
MrSterdy <mrsterdy@gmail.com>
2023-08-24 20:39:08 +0400

    Ignore no-non-null-assertion

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -28,6 +28,7 @@ module.exports = {
         }
     ],
     rules: {
-        "@typescript-eslint/ban-ts-comment": "off"
+        "@typescript-eslint/ban-ts-comment": "off",
+        "@typescript-eslint/no-non-null-assertion": "off"
     }
 };
